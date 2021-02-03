73f6d0744d68534c5b52245d498454ee53f84590
florentdesmarets <florent.desmarets@gmail.com>
2021-02-03 16:32:03 +0100

    home page done

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import AdminList from './components/admin/AdminList';
 import {
   BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
-
+import Footer from './components/footer/Footer'
+import Home from './components/home/Home'
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+          <Switch>
+            <Home/>
+          </Switch>
+        <Footer/>
       </Router>
     </div>
   );
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+    return(
+   
+        <div className="footer-container" >
+            <h3 className="made">Made with 🍆 for WildCodeSchool Orléans</h3>
+        </div>
+      
+       
+    );
+}
