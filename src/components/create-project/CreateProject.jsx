ca6b52c5b8f4b18dd07cba104a0e9aa416d409b1
florentdesmarets <florent.desmarets@gmail.com>
2021-02-11 23:27:35 +0100

    fixed

--- src/components/create-project/CreateProject.jsx
+++ src/components/create-project/CreateProject.jsx
@@ -6,28 +6,61 @@ import './../create-project/CreateProject.css'
 import {Link} from "react-router-dom";
 
 const CreateProject = () => {
-    
-    const handleSubmited = () => {
-        axios.post(`${FETCH}/projects`)
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-        }
-    
+
+    const [file, setFile] = useState('');
+    const [fileName, setFileName] = useState('');
+    const [uploadedFile, setUploadedFile] = useState({});
+    const [titleFile, setTitleFile] = useState('');
+  
+            const onChangeText = e => {
+              setTitleFile(e.target.value);
+            }
+
+            const onChange = e => {
+              setFile(e.target.files[0]);
+              setFileName(e.target.files[0].name);
+            }
+          
+            const handleSubmit = async e => {
+              e.preventDefault();
+              const formData = new FormData();
+              formData.append('file', file)
+          
+              try {
+                const res = await axios.post(`${FETCH}/upload`, formData, {
+                  headers: {
+                    'Content-Type': 'multipart/form-data'
+                  }
+                });
+          
+                const { fileName, filePath } = res.data
+          
+                setUploadedFile({ fileName, filePath});
+          
+              } catch(err) {
+               console.log(err)
+              }
+              axios.post(`${FETCH}/projects`,{project_image : fileName, project_title : titleFile })
+              .then(res => {
+                console.log(res);
+                console.log(res.data);
+              })
+            };
 
     return (
         <div className="create-project-container">
             <video className="video-about" src={`${DOMAIN_URL}/assets/video-true2.mp4`} autoPlay muted loop />
             <h1 className="main-title-create-project ">Create project</h1>
-            <form className="form-create-project" onSubmit={handleSubmited} action={`${FETCH}/projects`} method="post" target="BLANK">
+            <form className="form-create-project" onSubmit={handleSubmit} >
                     <div className="label-text-container">
                         <label className="label-text" for="project_title">Project Name</label>
-                        <input type="text" id="project_title" name="project_title"/>
+                        <input type="text" id="project_title" onChange={onChangeText} name="project_title"/>
                     </div>
                     <div className="button-container">
                     <label className="label-text" for="project_image">Upload photo</label>
-                        <input type="file" accept="image/png, image/jpeg, image/jpg" id="project_image" name="project_image" className="label-text"/>
-                        <button className="label-text-create-project">Add project</button>
+                        <input type="file" onChange={onChange} accept="image/png, image/jpeg, image/jpg" id="project_image" name="project_image" className="label-text"/>
                     </div>
+                    <button type="submit" className="label-text-create-project">Add project</button>
                 </form>
         </div>
     )
